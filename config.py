7bbaab3421060596fbd0e1726b2bef28b545c117
oalcabes <50996925+oalcabes@users.noreply.github.com>
2019-08-29 11:09:24 -0500

    Add files via upload

--- config.py
+++ config.py
@@ -5,13 +5,15 @@ Created on Wed Jul  3 13:37:11 2019
 @author: oalcabes
 """
 
+#CONFIGURATION FILE - EDIT THIS VALUES BEFORE RUNNING ValRE
+
 ### PATHFILES ###
 #FORMAT: output directories in the form of a string
 #EX. '.\Mag4_output'
 #OTHER NOTES: Write the directory however you would on your own computer.
 #The current working directory is the directory you have placed the ValRE package in.
-#If you do not have dates specified in the names of your model files, ValRE will NOT be
-#able to function!
+#If you do not have dates specified in the names of your model files, ValRE will NOT
+#be able to read them in!
 
 #model output path:
 model_path = '..\SEPSTER_output'
@@ -23,7 +25,6 @@ obs_path = '..\GOES_output'
 #FORMAT: string
 #EX. 'MAG4'
 #OTHER NOTES: this will only be used for report-writing purposes
-
 model_name = 'SEPSTER'
 
 
@@ -31,9 +32,10 @@ model_name = 'SEPSTER'
 #FORMAT: string
 #EX: 'GOES-08'
 #OPTIONS: GOES-08, -10, -11, -12, -13, -14 -15, SEPEM
-#OTHER NOTES: ValRE automatically reads in the instrument(s) used in your observation
-#files. However, if you'd like to manually specify the instrument(s)
-#used for validation, comment-in the line below:
+#OTHER NOTES: ValRE automatically chooses the correct GOES observational instrument to
+#use for your given start and end dates. However, if you'd like to specify one instrument
+#you'd like to use for the entire validation, or if you are using instrumental data
+#you've created yourself, comment-in the line below:
 #instrument = 'GOES-15'
 
 
@@ -62,8 +64,9 @@ end_day = 24
 #OTHER NOTES: the amount of days before the event that a model may have predictions for.
 #For example, if your model can only predict an event one day before it happens, 
 #put in one. If your model has the potential to predict an event 5 days before it
-#happens, put in 5. If your model can only predict hours before, this parameter should be
-#equal to 1.
+#happens, put in 5. The smallest value is 1 day, so if your model only has the
+#potential to predict an event a few hours before it happens, leave this value
+#as 1.
 days_before_event = 1
 
 
@@ -74,24 +77,23 @@ days_before_event = 1
 #value in energy_thresholds is 10, the first value of the pfu_thresholds must be the
 #corresponding flux value of 10. If the model you are using is probabilistic, you may
 #comment out pfu_threshold; similarly, if the model you are using is not probabilistic,
-#you may comment out prob_threshold. If prob_threshold is commented out for a
-#probabilistic model, ValRE will assume that the model output files contain probability
-#thresholds and will read them in instead.
-#Finally, ValRE will check if a model does not have output for each threshold and
-#if it doesn't simply won't create a report for that threshold.
+#you may comment out prob_threshold. However, leaving all values commented in, even if
+#unused, will not be a problem. Finally, ValRE will check if a model does not have output
+# for each threshold and if it doesn't simply won't create a report for that threshold.
 
 #threshold(s) (in MeV)
 energy_threshold = [10,100]
 #threshold(s) (in pfu)
 pfu_threshold = [10,1]
-#probabilisitic threshold (ONLY ONE VALUE FOR ALL THRESHOLDS)
-prob_threshold = 0.25 #consider this
+#probabilistic threshold (ONLY ONE VALUE FOR ALL THRESHOLDS)
+prob_threshold = 0.25
 
 
 ### REPORTS ###
 #FORMAT: True or False boolean
 #OTHER NOTES: this is not either/or, both can be marked True if you'd like both reports
-#and both can be marked False if you'd like none.
+#and both can be marked False if you'd like none (although you probably want one
+#otherwise idk why you're running this).
 
 #would you like a version of the report in PDF form? True if yes, False if no
 PDF_report = True
@@ -104,11 +106,11 @@ JSON_report = True
 #FORMAT: lists of integers
 #EX. [0,5]
 #OTHER NOTES: lists must be the same length as energy_threshold determined
-#in the thresholds category above, and each value is the amount of manual hits,
+#in the thresholds category above, and each value is the amount of hits,
 #misses, correct negatives, or false alarms for the corresponding energy_threshold
 #that has been calculated manually. These values can be added to if there
 #are particular events that ValRE is not able to use model and/or observational
-#output to calculate, but you would still like to be included for calculations
+#output to calculate, but you would still like them to be included for calculations
 #of metric scores.
 
 #hits calculated manually
@@ -123,7 +125,7 @@ man_false_alarms = [0,0]
 ### NO MODEL FILE ###
 #FORMAT: string
 #OPTIONS: 'all_clear' or 'nothing'
-#OTHER NOTES: if no_mode_file = 'all_clear', not having model files for a given event
+#OTHER NOTES: if no_mod_file = 'all_clear', not having model files for a given event
 #will be read as equivalent to a forecast all clear. If no_mod_file = 'nothing',
 #ValRE will skip all observation files that don't have corresponding model files. 
-no_mod_file = 'all_clear'
+no_mod_file = 'nothing'
